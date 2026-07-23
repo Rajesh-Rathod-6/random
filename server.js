6597aa3efcc4348f6bf250fb00c274466cfc7558
Rajesh Rathod <rajeshrathod5617@gmail.com>
2026-07-23 06:21:05 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-//hello .
+// let n = 5;
+
+// for(let i=0;i<=n; i++){
+//     console.log("hello",i);
+// }
+
+let argv = process.argv;
+
+for(let i=2; i<argv.length; i++){
+    console.log("hello and welcome to",argv[i]);
+}
